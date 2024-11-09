3051e2150321ffc91adab1715633401a1f402735
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-09 14:31:22 +0000

    Add API in javascript and check the requested data is being received

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const apiKey = "1e90deddd67ef6bc392f9c32";
+const baseCode = "GBP"
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCode}`;
+
+fetch(apiURL)                        // retrieve all data from this url
+    .then(function (response) {          // this gets called when the fetch function returns its data
+        return response.json();         // convert the relevant part of the page to a JS object
+    }).then(function (object) {          // this gets called when the conversion is complete
+        console.log(object);
+});
