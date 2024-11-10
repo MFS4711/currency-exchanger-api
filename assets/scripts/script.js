e6ef8118819d7882f626ebc040679b9f524be5da
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-10 12:01:12 +0000

    Add basic javascript to retrieve date from APi and display whent he button is clicked

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,46 @@
-// document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
-//     const buttonElement = document.getElementById("button");
-//     buttonElement.addEventListener("click", function() {
+    const apiKey = "1e90deddd67ef6bc392f9c32";
 
-//     });
+    const buttonElement = document.getElementById("button");
+    buttonElement.addEventListener("click", function() {
 
-// });
+        const baseCode = document.getElementById("currency-input-1").value;
+        const targetCode = document.getElementById("currency-input-2").value;
+        const amount = document.getElementById("amount").value;
+        const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCode}/${targetCode}/${amount}`;
 
-// const apiKey = "1e90deddd67ef6bc392f9c32";
-// const baseCode = "GBP"
-// const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCode}`;
+        fetch(apiURL)                        // retrieve all data from this url
+            .then(function (response) {          // this gets called when the fetch function returns its data
+                return response.json();         // convert the relevant part of the page to a JS object
+            }).then(function (object) {          // this gets called when the conversion is complete
+                console.log(object);
+                displayExchangeRates(object);
+        });
 
-// fetch(apiURL)                        // retrieve all data from this url
-//     .then(function (response) {          // this gets called when the fetch function returns its data
-//         return response.json();         // convert the relevant part of the page to a JS object
-//     }).then(function (object) {          // this gets called when the conversion is complete
-//         console.log(object);
-//         displayExchangeRates(object);
-// });
+    });
 
-// function displayExchangeRates(object) {
-//     let ratesArea = document.getElementById("rates-area");
-//     ratesArea.innerText = object.conversion_rates.USD;
-// }
+});
+
+
+
+
+
+function displayExchangeRates(object) {
+
+    let requestedAmount = document.getElementById("amount").value;
+    let baseCode = object.base_code;
+    let targetCode = object.target_code;
+    let conversionRate = object.conversion_rate;
+    let conversionResult = object.conversion_result;
+
+    let requestedRates = document.getElementById("requested-rates");
+
+    requestedRates.innerHTML = `
+        <p><strong>Requested Amount:</strong> ${requestedAmount} ${baseCode}</p>
+        <p><strong>Converted Amount:</strong> ${conversionResult} ${targetCode}</p>
+        <p><strong>The current exchange rate from ${baseCode} to ${targetCode} is </strong> ${conversionRate}</p>
+        <h5>Correct as of:</h5>
+        <h5>Next Update:</h5>
+        `
+}
