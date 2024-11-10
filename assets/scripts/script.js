ed7a00001c3fbfcf7a1faf482913c347986fdb14
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-10 11:31:50 +0000

    Add basic html structure for response and comment out javascript for now

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,25 @@
-const apiKey = "1e90deddd67ef6bc392f9c32";
-const baseCode = "GBP"
-const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCode}`;
+// document.addEventListener("DOMContentLoaded", function() {
 
-fetch(apiURL)                        // retrieve all data from this url
-    .then(function (response) {          // this gets called when the fetch function returns its data
-        return response.json();         // convert the relevant part of the page to a JS object
-    }).then(function (object) {          // this gets called when the conversion is complete
-        console.log(object);
-        displayExchangeRates(object);
-});
+//     const buttonElement = document.getElementById("button");
+//     buttonElement.addEventListener("click", function() {
 
-function displayExchangeRates(object) {
-    let ratesArea = document.getElementById("rates-area");
-    ratesArea.innerText = object.conversion_rates.USD;
-}
+//     });
+
+// });
+
+// const apiKey = "1e90deddd67ef6bc392f9c32";
+// const baseCode = "GBP"
+// const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCode}`;
+
+// fetch(apiURL)                        // retrieve all data from this url
+//     .then(function (response) {          // this gets called when the fetch function returns its data
+//         return response.json();         // convert the relevant part of the page to a JS object
+//     }).then(function (object) {          // this gets called when the conversion is complete
+//         console.log(object);
+//         displayExchangeRates(object);
+// });
+
+// function displayExchangeRates(object) {
+//     let ratesArea = document.getElementById("rates-area");
+//     ratesArea.innerText = object.conversion_rates.USD;
+// }
